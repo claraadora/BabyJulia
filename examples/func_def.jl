e438eda996513cb3adaab80891b67d14ce9b9393
Johanna <johannagwan@gmail.com>
2022-03-07 17:15:38 +0800

    test: add more examples for func_def

--- examples/func_def.jl
+++ examples/func_def.jl
@@ -3,3 +3,13 @@ function myplus(x, y)
     y::Int64 = 10
     return x
 end 
+
+function myplus(x::Int64)
+    x = 3
+end 
+
+function myplus(x::Int64, y::String)::String
+    x = 3 
+    y = "Hello!"
+    return y 
+end 
