38eff4d27b80f3e83406761f1ae97d391992cbb3
Clara <claraadora@gmail.com>
2022-03-16 01:42:05 +0800

    chore: add comment

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -95,7 +95,7 @@ class NodeGenerator implements BabyJuliaVisitor<Node> {
     const ctx = temp_ctx.STRING();
     return {
       type: "StringLiteral",
-      value: ctx.text.replace(/['"]+/g, ""),
+      value: ctx.text.replace(/['"]+/g, ""), // TODO: hacky
     };
   }
 
